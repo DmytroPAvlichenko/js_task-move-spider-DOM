4c69660cc4fe0ec5f632ece8359ff401a875e45a
dimon <80713na@gmail.com>
2025-04-06 03:26:14 +0300

    move

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,21 @@
 'use strict';
 
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
 document.addEventListener('click', (e) => {
-  // write code here
+  const wallRect = wall.getBoundingClientRect();
+
+  let clickTop = e.clientY - wallRect.top;
+  let clickLeft = e.clientX - wallRect.left;
+
+  clickLeft = Math.max(0, clickLeft);
+  clickTop = Math.max(0, clickTop);
+
+  // Перевірка меж для правої та нижньої сторін
+  clickLeft = Math.min(350, clickLeft);
+  clickTop = Math.min(350, clickTop);
+
+  spider.style.top = `${clickTop}px`;
+  spider.style.left = `${clickLeft}px`;
 });
